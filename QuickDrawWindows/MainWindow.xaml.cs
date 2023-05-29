d5c3b5227a00f889eb1b3621ba32a96ed091e1fb
Matthew Fraser <1923697+blendermf@users.noreply.github.com>
2023-05-28 20:11:16 -0400

    Added back titlebar animation with plans for controls there
    Now using AppWindow.TitleBar which hopefully should work better

--- QuickDrawWindows/MainWindow.xaml.cs
+++ QuickDrawWindows/MainWindow.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,9 +13,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,20 +29,48 @@ namespace QuickDraw
     /// </summary>
     public sealed partial class MainWindow : WinUIEx.WindowEx
     {
-
+        private AppWindow m_AppWindow;
         public MainWindow()
         {
             this.InitializeComponent();
-            this.ExtendsContentIntoTitleBar = true;
-            this.SetTitleBar(this.AppTitleBar);
+
+            m_AppWindow = GetAppWindowForCurrentWindow();
+
+            var titlebar = this.AppWindow.TitleBar;
+            titlebar.ExtendsContentIntoTitleBar = true;
+            titlebar.ButtonBackgroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonBackground"]).Color;
+            titlebar.ButtonHoverBackgroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonBackgroundPointerOver"]).Color;
+            titlebar.ButtonPressedBackgroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonBackgroundPressed"]).Color;
+            titlebar.ButtonInactiveBackgroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionBackgroundDisabled"]).Color;
+
+            titlebar.ButtonForegroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonStroke"]).Color;
+            titlebar.ButtonHoverForegroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonStrokePointerOver"]).Color;
+            titlebar.ButtonPressedForegroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionButtonStrokePressed"]).Color;
+            titlebar.ButtonInactiveForegroundColor = ((SolidColorBrush)Application.Current.Resources["WindowCaptionForegroundDisabled"]).Color;
+
+            titlebar.PreferredHeightOption = TitleBarHeightOption.Standard;
 
             this.MainFrame.Navigate(typeof(MainPage));
         }
 
         public void NavigateToSlideshow()
         {
+            var titlebar = this.AppWindow.TitleBar;
+            var delay = TimeSpan.Parse((string)Application.Current.Resources["ControlFastAnimationDuration"]);
+            Task.Run(async delegate { 
+                await Task.Delay(delay);
+                titlebar.PreferredHeightOption = TitleBarHeightOption.Tall;
+            });
+
             VisualStateManager.GoToState(this.AppTitleBar, "SlideLayout", true);
             this.MainFrame.Navigate(typeof(SlidePage), null, new SlideNavigationTransitionInfo { Effect = SlideNavigationTransitionEffect.FromRight });
+        }
+
+        private AppWindow GetAppWindowForCurrentWindow()
+        {
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId wndId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            return AppWindow.GetFromWindowId(wndId);
         }
     }
 }
